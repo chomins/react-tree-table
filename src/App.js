ccec5a06389757ab32c5fe087106b0e8381b16bf
chominsu <cms4317@naver.com>
2022-06-21 13:07:13 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Tree from "./components/Tree";
+import FileExplorer from "./components/FileExplorer";
+function App() {
+  return (
+    <div className="App">
+      <FileExplorer/>
+    </div>
+  );
+}
+
+export default App;
